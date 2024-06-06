a004c5ddb769eb3fc547e601bf85533afdde4b5f
iqra manzoor <iqra.manzoor@rabobank.nl>
2024-06-06 22:28:22 +0100

    assessment task..

--- app/src/main/java/com/example/bryjassessmenttest/MainActivity.kt
+++ app/src/main/java/com/example/bryjassessmenttest/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.bryjassessmenttest
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +19,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,6 +28,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bryjassessmenttest.ui.theme.BryjAssessmentTestTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,22 +57,37 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@SuppressLint("SuspiciousIndentation")
 @Composable
 fun SimpleListingView(){
+
+    val scope = rememberCoroutineScope()
+    var concatItemList = mutableListOf<String>()
+
         LazyColumn(Modifier.fillMaxSize()) {
             items(30) {
-                RowItems(item = it)
+                RowItems(item = it, onClickItem = { itemText->
+                    scope.launch {
+//                        delay(3000)
+                        concatItemList.add(itemText)
+                        concatItemList.map {
+                            Log.e("item :", it)
+                        }
+                    }
+                })
             }
     }
 }
 
 @Composable
-fun RowItems(item : Int){
+fun RowItems(item : Int,onClickItem : (String) -> Unit){
     Column(modifier= Modifier.height(50.dp),
         verticalArrangement = Arrangement.Center,
     ) {
         Text(
-            modifier = Modifier.padding(start = 10.dp),
+            modifier = Modifier
+                .padding(start = 10.dp)
+                .clickable { onClickItem("Hello $item") },
             text = "Hello $item",
             style = MaterialTheme.typography.bodyMedium.copy(
                 fontWeight = FontWeight.Medium,
